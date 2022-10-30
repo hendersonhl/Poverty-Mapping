baa90f18abca7d4555c1d56d49940b5ccbf16233
Heath Henderson <hendersonhl@gmail.com>
2022-10-30 13:25:20 -0500

    Add R-squared results

--- Code/results_rsquared.py
+++ Code/results_rsquared.py
@@ -10,7 +10,7 @@ outpath = '/Users/hendersonhl/Documents/Articles/Poverty-Mapping/Results/'
 # Import true poverty indicators and reshape
 # Note: All calculations are based on municipality-level data
 true = pd.read_csv(inpath + 'true_mun.csv', header = 0)
-true = true.rename(columns={'MiMun': 'muni'})
+true = true.rename(columns={'mimun': 'muni'})
 true = true[['muni', 'poor']]
 results = pd.DataFrame(np.repeat(true.values, 500, axis=0), columns=true.columns)  
 results['sim_sample'] = np.tile(np.arange(1,501), true.shape[0])
@@ -53,29 +53,33 @@ model = model + ['eb', 'uc']
 rsquared = pd.DataFrame()
 rsquared['sim_sample'] = np.arange(1,501)
 for i in model:     # Loop over all models and samples
+    print(i)
     true = []
+    constrain = []
     direct = []
     for j in range(1,501):
         data = results[(results['sim_sample'] == j) & (results['svysample'] == 1)]
-        y = data['poor']
+        y = data['poor']      # Get true R-squared
         X = data[i]
         X = sm.add_constant(X)
         reg = sm.OLS(y, X).fit()
         true.append(reg.rsquared)
-        y = data['dpoor']
+        y_bar = y.mean()    # Get constrained R-squared
+        ss_tot = ((y - y_bar)**2).sum()
+        ss_res = ((y - data[i])**2).sum()
+        constrain.append(1 - (ss_res/ss_tot))
+        y = data['dpoor']    # Get direct R-squared
         reg = sm.OLS(y, X).fit()
         direct.append(reg.rsquared)
     name1 = i + '_true'
-    name2 = i + '_direct'
+    name2 = i + '_constrain'
+    name3 = i + '_direct'
     rsquared[name1] = true
-    rsquared[name2] = direct
+    rsquared[name2] = constrain
+    rsquared[name3] = direct
     
 # Save results
 rsquared.to_csv(outpath + 'results_rsquared.csv', index = False)
-
-    
-
-    
     
     
 
