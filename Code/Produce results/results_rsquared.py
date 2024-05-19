2df30ef886f93bcf208689c7610fee5905a6e18f
Paul Corral <38434152+pcorralrodas@users.noreply.github.com>
2024-05-19 10:16:43 +0000

    Updated final results

--- Code/Produce results/results_rsquared.py
+++ Code/Produce results/results_rsquared.py
@@ -29,8 +29,10 @@ model = ['gb_census_mun', 'gb_gis_mun', 'gb_all_mun', 'gb_census_psu',
          'bart_census_mun', 'bart_gis_mun', 'bart_all_mun', 'bart_census_psu', 
          'rf_census_mun', 'rf_gis_mun', 'rf_all_mun', 'rf_census_psu', 
          'lasso_census_mun', 'lasso_gis_mun', 'lasso_all_mun', 'lasso_census_psu', 
-         'ols_census_mun', 'ols_gis_mun', 'ols_all_mun', 'ols_census_psu']
+         'ols_census_mun', 'ols_gis_mun', 'ols_all_mun', 'ols_census_psu', 
+         'gb_census_psu_wc', 'gb_gis_mun_ntl', 'gb_all_mun_ntl']
 for i in model:
+    print(i)
     res = pd.read_csv(outpath + i + '.csv', header = 0)
     cols = list(res)[1:]    # Reshape wide to long
     res = pd.melt(res, id_vars='muni', value_vars=cols)
